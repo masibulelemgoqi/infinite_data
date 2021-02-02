f8470c212417ecbee8daa1d0558383b0083cf5d4
Simamkele Ndabeni <43241594+VernonVerns@users.noreply.github.com>
2021-02-02 10:43:57 +0200

    Working on previous temp

--- lib/views/widgets/previous_temp.dart
+++ lib/views/widgets/previous_temp.dart
@@ -6,95 +6,93 @@ import 'package:infinite_data/models/class/screening.dart';
 
 Widget makeTemperature({clientId}) {
   Screening _screening = Screening();
-  return Center(
-    child: Container(
-      padding: EdgeInsets.all(10.0),
-      width: 150.0,
-      height: 150.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100.0),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          colors: [
-            darkBlue,
-            mainBlue,
-          ],
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey[350],
-            blurRadius: 10.0,
-            offset: Offset(5, 10),
-          )
+  return Container(
+    padding: EdgeInsets.all(10.0),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        colors: [
+          darkBlue,
+          mainBlue,
         ],
       ),
-      child: StreamBuilder(
-          stream: _screening.getScreening(clientId),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return loader();
-            }
-
-            List<DocumentSnapshot> docs = snapshot.data.docs;
-            if (docs.length == 0) {
-              return Center(
-                child: Text(
-                  'No record',
-                  style: GoogleFonts.roboto(
-                    textStyle: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              );
-            }
-
-            _screening.populateScreening(docs[0]);
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      _screening.temperature.toStringAsFixed(1),
-                      style: GoogleFonts.roboto(
-                        textStyle: TextStyle(
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '\u2103',
-                      style: GoogleFonts.roboto(
-                        textStyle: TextStyle(
-                          fontSize: 35.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  'Last Temperature',
-                  style: GoogleFonts.roboto(
-                    textStyle: TextStyle(
-                      fontSize: 12.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                )
-              ],
-            );
-          }),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey[350],
+          blurRadius: 10.0,
+          offset: Offset(5, 10),
+        )
+      ],
     ),
+    child: StreamBuilder(
+        stream: _screening.getScreening(clientId),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return loader();
+          }
+
+          List<DocumentSnapshot> docs = snapshot.data.docs;
+          if (docs.length == 0) {
+            return Center(
+              child: Text(
+                'No record',
+                style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            );
+          }
+
+          _screening.populateScreening(docs[0]);
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Wrap(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Savoy Spar',
+                        style: GoogleFonts.roboto(
+                          textStyle: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Today',
+                        style: GoogleFonts.roboto(
+                          textStyle: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '56.8',
+                        style: GoogleFonts.roboto(
+                          textStyle: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          );
+        }),
   );
 }
